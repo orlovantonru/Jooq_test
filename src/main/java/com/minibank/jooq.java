6bcf318f42d4e068b0ece3e75ba050864cd70bd5
orlov_anton <orlov_anton@mail.ru>
2023-10-02 23:21:41 +0300

    JOOQ class generating

---
+++ src/main/java/com/minibank/jooq.java
@@ -0,0 +1,12 @@
+package com.minibank;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+public class jooq {
+
+    public static void main(String[] args) {
+        SpringApplication.run(jooq.class, args);
+    }
+}
